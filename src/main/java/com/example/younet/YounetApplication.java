33cd1c98f47e360549572dda01f411205db7fa3d
Minji Kim <minji1289@naver.com>
2024-01-03 16:16:47 +0900

    초기 설정파일

---
+++ src/main/java/com/example/younet/YounetApplication.java
@@ -0,0 +1,13 @@
+package com.example.younet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YounetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/younet/YounetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.younet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
