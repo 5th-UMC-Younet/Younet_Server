1020a6cb236e45455dc1e8d5074d7655404b1845
정민규 <fastdodge7@naver.com>
2024-01-24 20:38:46 +0900

    [feat] 댓글 / 대댓글 도메인(엔티티) 파일 생성 #4

---
+++ src/main/java/com/example/younet/domain/Reply.java
@@ -0,0 +1,42 @@
+package com.example.younet.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "communityProfile_id")
+    private CommunityProfile communityProfile;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+}
